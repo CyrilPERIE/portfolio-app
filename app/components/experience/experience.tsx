40f6cb93c519f0389b3cf6168ab9d0deaf2e1a45
Cyril <cperie@oxyl.fr>
2023-12-27 08:17:54 +0100

    Experience scrollable

--- app/components/experience/experience.tsx
+++ app/components/experience/experience.tsx
@@ -3,9 +3,9 @@ import ExperienceCard from "./experience-card";
 
 export default function Experience() {
     return(
-        <div className="h-section bg-green-900 flex justify-center">
-            <div className=" w-3/4 h-full bg-violet-500 flex flex-row overflow-y-scroll no-scrollbar">
-                <div className="w-1/2 h-full  sticky top-0">
+        <div className="min-h-section bg-green-900 flex justify-center">
+            <div className=" w-3/4 h-full bg-violet-500 flex flex-row">
+                <div className="w-1/2 h-screen sticky top-0">
                     <div className="h-full bg-[url('/assets/skyscraper.png')] bg-fit-y" />
                 </div>
                 <div className="w-2/3 h-full flex items-center flex-col">
--- app/components/projets/projets.tsx
+++ app/components/projets/projets.tsx
@@ -2,21 +2,16 @@ import { state } from "@/app/state";
 import ProjetCard from "./projet-card";
 
 export default function Projets() {
-    return(
-        <div className="h-section bg-[url('/assets/terre.jpg')] bg-repeat-x bg-fit-y">
-        <div className="h-1/5">
-            Terre
-        </div>
-        <div className="h-4/5 bg-[url('/assets/metro.png')] bg-repeat-x bg-fit-y">
-            <div>Tête de train</div>
-            {state.projects.map((projet) => (
-                <ProjetCard
-                key={projet.href}
-                {...projet}
-                />
-            ))}
-            <div>Queue de train</div>
-        </div>
-        </div>
-    )
+  return (
+    <div className="min-h-section flex flex-col">
+      <div className="h-section bg-[url('/assets/terre.jpg')]">Terre</div>
+      <div className="h-project bg-[url('/assets/metro.png')] bg-repeat-x bg-fit-y">
+        <div>Tête de train</div>
+        {state.projects.map((projet) => (
+          <ProjetCard key={projet.href} {...projet} />
+        ))}
+        <div>Queue de train</div>
+      </div>
+    </div>
+  );
 }
