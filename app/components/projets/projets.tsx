1ca114c5f1ded9114a9f129bfd952bb72824548c
Cyril <cperie@oxyl.fr>
2024-01-18 12:01:53 +0100

    WIP

--- app/components/projets/projets.tsx
+++ app/components/projets/projets.tsx
@@ -4,7 +4,7 @@ import ProjetCard from "./projet-card";
 export default function Projets() {
   return (
     <div className="min-h-section flex flex-col">
-      <div className="h-section bg-[url('/assets/terre.jpg')]">Terre</div>
+      <div className="h-[250vh] bg-[url('/assets/terre.jpg')]">Terre</div>
       <div className="h-project bg-[url('/assets/metro.png')] bg-repeat-x bg-fit-y">
         <div>Tête de train</div>
         {state.projects.map((projet) => (
